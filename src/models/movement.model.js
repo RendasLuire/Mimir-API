1f1045e235f88440dc1f819d3b4ad0b896ce2a25
RendasLuire <andres.aguirre_o@hotmail.com>
2024-03-27 09:57:22 -0600

    model movement

---
+++ src/models/movement.model.js
@@ -0,0 +1,13 @@
+import { Mongoose } from "mongoose";
+
+const movementSchema = new Mongoose.Schema(
+    {
+        user: String,
+        computer: String,
+        type: String,
+        date: Date,
+        Description: String
+    }
+)
+
+export default Mongoose.model('Movement', movementSchema)
