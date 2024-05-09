d291926ef67d576fe6c3380e47bafc8d4f66786b
RendasLuire <andres.aguirre_o@hotmail.com>
2024-05-09 16:18:27 -0600

    diferents enviroments

--- src/app.js
+++ src/app.js
@@ -18,7 +18,19 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME });
+let dataBase = process.env.MONGO_DB_NAME;
+let port = process.env.PORT.trim() || 3000;
+
+if (process.env.NODE_ENV.trim() == "development") {
+  dataBase = process.env.MONGO_DB_NAME_DEV;
+  port = process.env.PORT_DEV.trim();
+}
+if (process.env.NODE_ENV.trim() == "test") {
+  dataBase = process.env.MONGO_DB_NAME_TEST;
+  port = process.env.PORT_TEST.trim();
+}
+
+mongoose.connect(process.env.MONGO_URL, { dbName: dataBase });
 const db = mongoose.connection;
 
 app.use("/api/user", userRoutes);
@@ -29,7 +41,14 @@ app.use("/api/persons", personRoutes);
 app.use("/storages", storageRoutes);
 app.use("/api/reports", pdfRoutes);
 
-const port = process.env.PORT || 3000;
+if (process.env.NODE_ENV.trim() == "development") {
+  console.log("Cargando Informacion....");
+}
+if (process.env.NODE_ENV.trim() == "test") {
+  console.log("Cargando Informacion....");
+}
+
+//Crear un usuario inicial
 
 app.listen(port, () => {
   console.log(`Servidor iniciado en el puerto ${port}`);
