61b8682bef39035e43f7455e79f4340f1f4fef02
Andres Uriel Aguirre Ocampo <andres.aguirre_o@hotmail.com>
2024-11-27 16:53:10 -0600

    Update setting.model.js

--- src/models/setting.model.js
+++ src/models/setting.model.js
@@ -5,41 +5,23 @@ const settingSchema = new mongoose.Schema({
     type: String,
     required: [true, "Name is required"],
     unique: true,
-    set: (info) => info?.toLowerCase() ?? "",
   },
   options: [
     {
       label: {
         type: String,
         required: [true, "Label is required"],
-        set: (value) => value?.toLowerCase() ?? "",
       },
       value: {
         type: String,
         required: [true, "Value is required"],
-        set: (value) => value?.toLowerCase() ?? "",
+      },
+      option: {
+        type: String,
+        default: "",
       },
     },
   ],
-});
-
-settingSchema.pre("save", function (next) {
-  if (this.name) {
-    this.name = this.name.toLowerCase();
-  }
-
-  if (this.options && this.options.length > 0) {
-    this.options.forEach((option) => {
-      if (option.label) {
-        option.label = option.label.toLowerCase();
-      }
-      if (option.value) {
-        option.value = option.value.toLowerCase();
-      }
-    });
-  }
-
-  next();
 });
 
 export default mongoose.model("Setting", settingSchema);
